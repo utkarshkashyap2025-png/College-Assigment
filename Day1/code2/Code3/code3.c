d72f7d90160b846efd9c54bc40847ada2fc80d50
Utkarsh Kashyap <utkarsh.kashyap2025@glbajajgroup.org>
2026-06-09 20:32:18 +0530

    Code 3 is completed

---
+++ Day1/code2/Code3/code3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,fact=1,i;
+    printf("Enter the value of n: ");
+    scanf("%d",&n); 
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("Factorial of %d is %d\n",n,fact);
+    return 0;
+}
