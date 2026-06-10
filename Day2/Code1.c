c6d16bf4f913f826670510dd332d2fe2f0ee74ed
Utkarsh Kashyap <utkarsh.kashyap2025@glbajajgroup.org>
2026-06-10 22:19:00 +0530

    Code 1 completed

--- Day2/Code1.c
+++ Day2/Code1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n , sum = 0 , rem = 0;
-    printf("Enter the number: ");       
-    scanf("%d", &n);
-    while (n != 0)
+    int n , sum = 0 , rem;
+    printf("Enter a number : ");
+    scanf("%d" , &n);   
+    while(n != 0)
     {
-        rem = n%10;
+        rem = n % 10;
         sum = sum + rem;
-        n = n/10;
+        n = n / 10;
     }
-    printf("Sum of digits: %d\n", sum);
+    printf("The sum of the digits is : %d" , sum);
     return 0;
 }
