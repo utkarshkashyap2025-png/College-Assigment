ca5d8a81f9494e94f16643c4264ac938b2386fb8
Utkarsh Kashyap <utkarsh.kashyap2025@glbajajgroup.org>
2026-06-10 10:09:15 +0530

    Code 1 is completed

---
+++ Day2/Code1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n , sum = 0 , rem = 0;
+    printf("Enter the number: ");       
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        rem = n%10;
+        sum = sum + rem;
+        n = n/10;
+    }
+    printf("Sum of digits: %d\n", sum);
+    return 0;
+}
